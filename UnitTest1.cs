0f2889a8ad77845dfd8245d44194cf9ff43fbfbd
Terje Sandstrøm <terje@hermit.no>
2020-05-21 20:29:38 +0200

    update

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -14,5 +14,17 @@ namespace binthing
         {
             Assert.Pass();
         }
+
+        [Test]
+        public void Test2()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void Test3()
+        {
+            Assert.Pass();
+        }
     }
 }
